fc61893b363ba73e78a91f87b316d74bbf25facb
Splines <dominic-plein@gmx.de>
2024-10-31 22:20:29 +0100

    Use boolean datatype (instead of any)

--- src/manimShell.ts
+++ src/manimShell.ts
@@ -81,7 +81,7 @@ export interface CommandExecutionEventHandler {
      * during Manim startup), the shell might not exist anymore after the
      * command was issued.
      */
-    onCommandIssued?: (shellStillExists: any) => void;
+    onCommandIssued?: (shellStillExists: boolean) => void;
 
     /**
      * Callback that is invoked when data is received from the active Manim
