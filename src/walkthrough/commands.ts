473562e6e05fbecd3b81ca6b0c4a39fec750b040
Splines <37160523+Splines@users.noreply.github.com>
2024-12-12 19:26:34 +0100

    Init Manim Notebook walkthrough (#90)
    * Init walkthrough with Manim installation check
    
    * Add open sample scene to walkthrough
    
    * Add preview cell SVG
    
    * Remove unknown property
    
    * Remove invalid key from openTextDocument
    
    * Fix media JSON key
    
    * Add open tab to Figma Preview Cell SVG
    
    * Add step to show all extension commands in walkthrough
    
    * Reexport svg without VSCode theme support
    
    * Add open walkthrough command
    
    * Improve header of section
    
    * Make `Preview Manim Cell` clickable in svg
    
    * Also make commands svg clickable
    
    * Improve wording in sample scene file
    
    * Add walkthrough step to show keyboard shortcuts
    
    * Add walkthrough step to open settings
    
    * Add walkthrough step to open wiki in browser
    
    * Add additional linebreak to warning box
    
    * Add hotkey comment to python walkthrough example
    
    * Extract the id of the extension from the context, in case the owner changes (as it will eventually)
    
    * Fix spelling mistake in Python sample file comments
    
    * Lint extension.ts file
    
    * Add accidentally deleted export scene command back
    
    * Directly call `self.frame` in Python sample code
    
    * Show the terminal for manim version printing
    
    * Remove wait() in example for quick cell execution
    
    ---------
    
    Co-authored-by: Ben Hoover <24350185+bhoov@users.noreply.github.com>

---
+++ src/walkthrough/commands.ts
@@ -0,0 +1,113 @@
+import * as vscode from 'vscode';
+import { ExtensionContext, window, workspace, commands, extensions } from 'vscode';
+import { Logger, Window } from '../logger';
+import { ManimShell } from '../manimShell';
+import fs from 'fs';
+import path from 'path';
+
+export function registerWalkthroughCommands(context: ExtensionContext) {
+    const checkManimVersionCommand = commands.registerCommand(
+        'manim-notebook-walkthrough.checkManimVersion', async () => {
+            Logger.info("💠 Command Check Manim Version requested");
+            await checkManimVersion();
+        });
+
+    const openSampleFileCommand = commands.registerCommand(
+        'manim-notebook-walkthrough.openSample', async () => {
+            Logger.info("💠 Command Open Sample File requested");
+            await openSampleFile(context);
+        });
+
+    const showAllCommandsCommand = commands.registerCommand(
+        'manim-notebook-walkthrough.showCommands', async () => {
+            Logger.info("💠 Command Show All Commands requested");
+            await showAllCommands();
+        }
+    );
+
+    const showKeyboardShortcutsCommand = commands.registerCommand(
+        'manim-notebook-walkthrough.showShortcuts', async () => {
+            Logger.info("💠 Command Show Keyboard Shortcuts requested");
+            await showKeyboardShortcuts();
+        }
+    );
+
+    const showSettingsCommand = commands.registerCommand(
+        'manim-notebook-walkthrough.showSettings', async () => {
+            Logger.info("💠 Command Show Settings requested");
+            await showSettings();
+        }
+    );
+
+    const openWikiCommand = commands.registerCommand(
+        'manim-notebook-walkthrough.openWiki', async () => {
+            Logger.info("💠 Command Open Wiki requested");
+            await openWiki();
+        }
+    );
+
+    context.subscriptions.push(
+        checkManimVersionCommand,
+        openSampleFileCommand,
+        showAllCommandsCommand,
+        showKeyboardShortcutsCommand,
+        showSettingsCommand,
+        openWikiCommand
+    );
+}
+
+async function checkManimVersion() {
+    const terminal = window.createTerminal("Manim Version");
+    terminal.show()
+    await new Promise((resolve) => setTimeout(resolve, 1500));
+    terminal.sendText("manimgl --version");
+}
+
+/**
+ * Opens a sample Manim file in a new editor that the user can use to get started.
+ * 
+ * @param context The extension context.
+ */
+async function openSampleFile(context: ExtensionContext) {
+    const sampleFilePath = path.join(context.extensionPath,
+        'src', 'walkthrough', 'sample_scene.py');
+    const sampleFileContent = fs.readFileSync(sampleFilePath, 'utf-8');
+
+    const sampleFile = await workspace.openTextDocument({
+        language: 'python',
+        content: sampleFileContent
+    });
+
+    await window.showTextDocument(sampleFile);
+}
+
+/**
+ * Opens the command palette with all the "Manim Notebook" commands.
+ */
+async function showAllCommands() {
+    await commands.executeCommand('workbench.action.quickOpen', '>Manim Notebook:');
+}
+
+/**
+ * Opens the keyboard shortcuts page in the settings.
+ */
+async function showKeyboardShortcuts() {
+    await commands.executeCommand(
+        'workbench.action.openGlobalKeybindings', "Manim Notebook");
+}
+
+/**
+ * Opens the settings page for the extension.
+ */
+async function showSettings() {
+    await commands.executeCommand(
+        'workbench.action.openSettings', "Manim Notebook");
+}
+
+/**
+ * Opens the GitHub wiki page for the extension.
+ */
+async function openWiki() {
+    const wikiUrl = "https://github.com/Manim-Notebook/manim-notebook/wiki";
+    await vscode.env.openExternal(vscode.Uri.parse(wikiUrl));
+}
